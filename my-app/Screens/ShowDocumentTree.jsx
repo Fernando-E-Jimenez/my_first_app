b20457c7d0d22247acef862a66ff9d19abcc81d9
Fernando-E-Jimenez <ferve91@gmail.com>
2020-11-24 17:22:54 -0300

    Estoy intentando hacer los commits pero me sale timedOut. Pude hardcodear la respuesta de la API y mostrarla en pantalla, lo que intento ahora es darle un poco de estilo para luego concentrarme en recibir el dato por el input y manejar los errores

--- my-app/Screens/ShowDocumentTree.jsx
+++ my-app/Screens/ShowDocumentTree.jsx
@@ -1,36 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native';
 
-export default function Login({navigation}, text){
+export default function Login(text){
 
-  const viewMain = navigation;
-  const [usuarios, setUsuarios] = useState([])
+  const [usuarios, setUsuarios] = useState([]);
 
   useEffect(() => {
-    obtenerDatos()
-  }, [])
+    obtenerDatos();
+  }, []);
 
   const obtenerDatos = async () => {
-    const data = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users = await data.json()
-    setUsuarios(users)
-  }
+    const data = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await data.json();
+    setUsuarios(users);
+  };
 
   return(
-    <View style={styles.container}>
-      <ul>
-        {
-          usuarios.map(item => (
-            <li key={item.id}>{item.name} - {item.email}</li>
-          ))
-        }
-      </ul>
-      <Button
-        title='Back' 
-        onPress={()=>this.navigation.navigate({viewMain})}>
-      </Button>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        data={usuarios}
+        renderItem={({item}) => (
+          <Text>
+            <Text> Id: {item.id}</Text>
+            <Text> Nombre: {item.name}</Text>
+            <Text> Username: {item.username}</Text>
+            <Text> Email: {item.email}</Text>
+            <Text> Address: </Text>
+            <Text> Street: {item.address.street}</Text>
+            <Text> City: {item.address.city}</Text>
+            <Text> Zipcode: {item.address.zipcode}</Text>
+          </Text>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -41,12 +43,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 20,
-  },
-  image: {
-    width: 850,
-    height: 250,
   },
 });
